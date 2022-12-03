bbd7f24414730d662dafd5786ec3c73b9a1ead55
docfate111 <tdwilliamsiv@gmail.com>
2022-12-03 09:54:56 -0800

    finally compiling

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,134 @@
-#[cfg(windows)]
-use std::ptr::write_volatile;
-use std::{fs, io::Read, path::PathBuf};
+use std::path::PathBuf;
 
 use libafl::{
-    bolts::{shmem::StdShmemProvider, shmem::UnixShMemProvider, current_nanos, rands::StdRand, tuples::tuple_list},
-    corpus::{OnDiskCorpus},
-    events::SimpleEventManager,
-    executors::{forkserver::ForkserverExecutor},
-    feedbacks::{CrashFeedback, MaxMapFeedback},
-    fuzzer::{Evaluator, Fuzzer, StdFuzzer},
-    inputs::GeneralizedInput,
-    monitors::MultiMonitor,
-    mutators::{
-        scheduled::StdScheduledMutator, GrimoireExtensionMutator,
-        GrimoireRandomDeleteMutator,
-        GrimoireStringReplacementMutator,
+    bolts::{
+        current_nanos,
+        rands::StdRand,
+        shmem::{ShMem, ShMemProvider, UnixShMemProvider},
+        tuples::{tuple_list, MatchName, Merge},
+        AsMutSlice,
     },
-    observers::{HitcountsMapObserver, StdMapObserver, TimeObserver},
-    schedulers::QueueScheduler,
+    corpus::{Corpus, InMemoryCorpus, OnDiskCorpus},
+    events::SimpleEventManager,
+    executors::{
+        forkserver::ForkserverExecutor,
+        HasObservers,
+    },
+    feedback_and_fast, feedback_or,
+    feedbacks::{CrashFeedback, MaxMapFeedback, TimeFeedback},
+    fuzzer::{Fuzzer, StdFuzzer},
+    inputs::BytesInput,
+    monitors::SimpleMonitor,
+    mutators::{scheduled::havoc_mutations, tokens_mutations, StdScheduledMutator, Tokens},
+    observers::{HitcountsMapObserver, MapObserver, StdMapObserver, TimeObserver},
+    schedulers::{IndexesLenTimeMinimizerScheduler, QueueScheduler},
     stages::mutational::StdMutationalStage,
-    state::StdState,
+    state::{HasCorpus, HasMetadata, StdState},
 };
 
-/// Coverage map with explicit assignments due to the lack of instrumentation
-static mut SIGNALS: [u8; 16] = [0; 16];
+/*/// The commandline args this fuzzer accepts
+#[derive(Debug, Parser)]
+#[command(
+    name = "forkserver_simple",
+    about = "This is a simple example fuzzer to fuzz a executable instrumented by afl-cc.",
+    author = "tokatoka <tokazerkje@outlook.com>"
+)]
+struct Opt {
+    #[arg(
+        help = "The instrumented binary we want to fuzz",
+        name = "EXEC",
+        required = true
+    )]
+    executable: String,
 
-/// Assign a signal to the signals map
-fn signals_set(idx: usize) {
-    unsafe { SIGNALS[idx] = 1 };
+    #[arg(
+        help = "The directory to read initial inputs from ('seeds')",
+        name = "INPUT_DIR",
+        required = true
+    )]
+    in_dir: PathBuf,
+
+    #[arg(
+        help = "Timeout for each individual execution, in milliseconds",
+        short = 't',
+        long = "timeout",
+        default_value = "1200"
+    )]
+    timeout: u64,
+
+    #[arg(
+        help = "If not set, the child's stdout and stderror will be redirected to /dev/null",
+        short = 'd',
+        long = "debug-child",
+        default_value = "false"
+    )]
+    debug_child: bool,
+
+    #[arg(
+        help = "Arguments passed to the target",
+        name = "arguments",
+        num_args(1..),
+        allow_hyphen_values = true,
+    )]
+    arguments: Vec<String>,
+
+    #[arg(
+        help = "Signal used to stop child",
+        short = 's',
+        long = "signal",
+        value_parser = str::parse::<Signal>,
+        default_value = "SIGKILL"
+    )]
+    signal: Signal,
 }
-
-/*fn is_sub<T: PartialEq>(mut haystack: &[T], needle: &[T]) -> bool {
-    if needle.is_empty() {
-        return true;
-    }
-    while !haystack.is_empty() {
-        if haystack.starts_with(needle) {
-            return true;
-        }
-        haystack = &haystack[1..];
-    }
-    false
-}*/
-
+*/
 #[allow(clippy::similar_names)]
 pub fn main() {
-    let mut initial_inputs = vec![];
-    for entry in fs::read_dir("./corpus").unwrap() {
-        let path = entry.unwrap().path();
-        let attr = fs::metadata(&path);
-        if attr.is_err() {
-            continue;
-        }
-        let attr = attr.unwrap();
+    const MAP_SIZE: usize = 65536;
 
-        if attr.is_file() && attr.len() > 0 {
-            println!("Loading file {:?} ...", &path);
-            let mut file = fs::File::open(path).expect("no file found");
-            let mut buffer = vec![];
-            file.read_to_end(&mut buffer).expect("buffer overflow");
-            let input = GeneralizedInput::new(buffer);
-            initial_inputs.push(input);
-        }
-    }
+    //let opt = Opt::parse();
 
+    let corpus_dirs: Vec<PathBuf> = [PathBuf::from("./corpus")].to_vec();
 
-     /*  // function to fuzz
-    let mut harness = |input: &GeneralizedInput| {
-        let target_bytes = input.target_bytes();
-        let bytes = target_bytes.as_slice();
+    // The unix shmem provider supported by AFL++ for shared memory
+    let mut shmem_provider = UnixShMemProvider::new().unwrap();
 
-        if is_sub(bytes, "fn".as_bytes()) {
-            signals_set(2);
-        }
-        ExitKind::Ok
-    };*/
+    // The coverage map shared between observer and executor
+    let mut shmem = shmem_provider.new_shmem(MAP_SIZE).unwrap();
+    // let the forkserver know the shmid
+    shmem.write_to_env("__AFL_SHM_ID").unwrap();
+    let shmem_buf = shmem.as_mut_slice();
 
     // Create an observation channel using the signals map
-    let observer = StdMapObserver::new("signals", unsafe { &mut SIGNALS });
+    let edges_observer = HitcountsMapObserver::new(StdMapObserver::new("shared_mem", shmem_buf));
+
+    // Create an observation channel to keep track of the execution time
     let time_observer = TimeObserver::new("time");
 
     // Feedback to rate the interestingness of an input
-    let mut feedback = MaxMapFeedback::new_tracking(&observer, false, true);
-
-    const MAP_SIZE: usize = 65536;
-    let mut shmem_provider = UnixShMemProvider::new().unwrap();
-    // The coverage map shared between observer and executor
-    let mut shmem = shmem_provider.new_shmem(MAP_SIZE).unwrap();
-    //let the forkserver know the shmid
-    shmem.write_to_env("__AFL_SHM_ID").unwrap();
-    let shmem_buf = shmem.as_mut_slice();
-    // write shared memory id to environment so Executor knows about it
-            // Create an observation channel using the signals map
-    let edges_observer = HitcountsMapObserver::new(StdMapObserver::new("shared_mem",
-                 shmem_buf));
-    let mut feedback = MaxMapFeedback::new_tracking(&edges_observer, true, true);
+    // This one is composed by two Feedbacks in OR
+    let mut feedback = feedback_or!(
+        // New maximization map feedback linked to the edges observer and the feedback state
+        MaxMapFeedback::new_tracking(&edges_observer, true, false),
+        // Time feedback, this one does not need a feedback state
+        TimeFeedback::new_with_observer(&time_observer)
+    );
 
     // A feedback to choose if an input is a solution or not
-    let mut objective = CrashFeedback::new();
+    // We want to do the same crash deduplication that AFL does
+    let mut objective = feedback_and_fast!(
+        // Must be a crash
+        CrashFeedback::new(),
+        // Take it onlt if trigger new coverage over crashes
+        MaxMapFeedback::new(&edges_observer)
+    );
 
     // create a State from scratch
     let mut state = StdState::new(
         // RNG
         StdRand::with_seed(current_nanos()),
         // Corpus that will be evolved, we keep it in memory for performance
-        OnDiskCorpus::new(PathBuf::from("./corpus")).unwrap(),
+        InMemoryCorpus::<BytesInput>::new(),
         // Corpus in which we store solutions (crashes in this example),
         // on disk so the user can get them after stopping the fuzzer
         OnDiskCorpus::new(PathBuf::from("./crashes")).unwrap(),
@@ -116,67 +140,61 @@ pub fn main() {
     )
     .unwrap();
 
-    /*if state.metadata().get::<Tokens>().is_none() {
-        state.add_metadata(Tokens::from([b"FOO".to_vec(), b"BAR".to_vec()]));
-    }*/
-
     // The Monitor trait define how the fuzzer stats are reported to the user
-    let monitor = MultiMonitor::new(|s| println!("{}", s));
+    let monitor = SimpleMonitor::new(|s| println!("{}", s));
+
     // The event manager handle the various events generated during the fuzzing loop
     // such as the notification of the addition of a new item to the corpus
     let mut mgr = SimpleEventManager::new(monitor);
-    //let mut mgr = SimpleEventManager::new(stats);
-    // A queue policy to get testcasess from the corpus
-    let scheduler = QueueScheduler::new();
+
+    // A minimization+queue policy to get testcasess from the corpus
+    let scheduler = IndexesLenTimeMinimizerScheduler::new(QueueScheduler::new());
 
     // A fuzzer with feedbacks and a corpus scheduler
     let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
 
-    //let generalization = GeneralizationStage::new(&observer);
+    // Create the executor for the forkserver
 
-    // Create the executor for an in-process function with just one observer
-    /*let mut executor = InProcessExecutor::new(
-        // not in scope anymore 
-        &mut harness,
-        tuple_list!(observer),
-        &mut fuzzer,
-        &mut state,
-        &mut mgr,
-    )
-    .expect("Failed to create the Executor");
-    */
-    let program = 
-    let mut executor = ForkserverExecutor::builder()
-                    .is_persistent(true)
-                    .build_dynamic_map(edges_observer, tuple_list!(time_observer))
-                    
-        .program("LD_LIBRARY_PATH=/home/t/Fuzzing/HDexecutor ./home/t/Fuzzing/HDexecutor/target/release/hdexecutor".to_string())
-                    .args(
-    &[String::from("@@"), "/home/t/Fuzzing/HDexecutor/btrfs.img".to_string(), "btrfs".to_string()])
-                    .shmem_provider(&mut shmem_provider)
-                    .coverage_map_size(MAP_SIZE).unwrap();
+    let mut tokens = Tokens::new();
+    let mut forkserver = ForkserverExecutor::builder()
+        .program("LD_LIBRARY_PATH=/home/t/Fuzzing/HDexecutor ./home/t/Fuzzing/HDexecutor/target/release/hdexecutor @@ /home/t/Fuzzing/HDexecutor/btrfs.img btrfs".to_string())
+        //.args([String::from("@@"), )
+        .shmem_provider(&mut shmem_provider)
+        .autotokens(&mut tokens)
+       // .parse_afl_cmdline(args)
+        .coverage_map_size(MAP_SIZE)
+        .build_dynamic_map(edges_observer, tuple_list!(time_observer))
+         .unwrap();
 
-    let grimoire_mutator = StdScheduledMutator::with_max_stack_pow(
-        tuple_list!(
-            GrimoireExtensionMutator::new(),
-            GrimoireStringReplacementMutator::new(),
-            GrimoireRandomDeleteMutator::new(),
-        ),
-        3,
-    );
-    
-    let mut stages = tuple_list!(
-        StdMutationalStage::new(grimoire_mutator)
-    );
-
-
-    for input in initial_inputs {
-        fuzzer
-            .evaluate_input(&mut state, &mut executor, &mut mgr, input)
-            .unwrap();
+    if let Some(dynamic_map_size) = forkserver.coverage_map_size() {
+        forkserver
+            .observers_mut()
+            .match_name_mut::<HitcountsMapObserver<StdMapObserver<'_, u8, false>>>("shared_mem")
+            .unwrap()
+            .downsize_map(dynamic_map_size);
     }
 
+    // In case the corpus is empty (on first run), reset
+    if state.corpus().count() < 1 {
+        state
+            .load_initial_inputs(&mut fuzzer, &mut forkserver, &mut mgr, &corpus_dirs)
+            .unwrap_or_else(|err| {
+                panic!(
+                    "Failed to load initial corpus at {:?}: {:?}",
+                    &corpus_dirs, err
+                )
+            });
+        println!("We imported {} inputs from disk.", state.corpus().count());
+    }
+
+    state.add_metadata(tokens);
+
+    // Setup a mutational stage with a basic bytes mutator
+    let mutator =
+        StdScheduledMutator::with_max_stack_pow(havoc_mutations().merge(tokens_mutations()), 6);
+    let mut stages = tuple_list!(StdMutationalStage::new(mutator));
+
     fuzzer
-        .fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr)
+        .fuzz_loop(&mut stages, &mut forkserver, &mut state, &mut mgr)
         .expect("Error in the fuzzing loop");
 }
