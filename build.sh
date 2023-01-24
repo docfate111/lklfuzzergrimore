866be415293b1b5daf85ada4808247b5450ee2f3
Thelford Williams <tdwilliamsiv@gmail.com>
2023-01-23 19:53:08 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+apt install -y bc make flex bison libncurses-dev libelf-dev libssl-dev
 git clone https://github.com/lkl/linux.git
-pushd linux
+cd linux
 export CFLAGS='-fsanitize-coverage=trace-pc-guard'
 export CXXFLAGS='-fsanitize-coverage=trace-pc-guard'
 sed -i -e 's/CFLAGS=/CFLAGS+=/' Makefile
-CONFIG_CLANG=y make -j`nproc`
-popd
+CONFIG_CLANG=y ARCH=lkl make -j`nproc`
+cd ..
 
 cp linux/tools/lkl/liblkl.a .
 unset CC
 unset CXX
 unset CFLAGS
 unset CXXFLAGS
-
-cargo +nightly build --release
+# i removed the +nigthly
+cargo build --release
 
 export CC=`pwd`/target/release/libafl_cc
 export CXX=`pwd`/target/release/libafl_cxx
